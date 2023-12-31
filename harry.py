c903f0dfa15fd21150a329df320e1e271729fa17
Aadhav Vijaya Saravanan <144838207+Aadhav-VijayaSaravanan@users.noreply.github.com>
2023-12-31 19:40:49 +0100

    Update harry.py

--- harry.py
+++ harry.py
@@ -1,6 +1,7 @@
 def main():
+    global a1, a2, a3, ratio, wantNum
     superscript = {'1': '¹', 'n': 'ⁿ', '-': '⁻'}
-    sequence = input("Input sequence (space-separated): ").split()
+    sequence = input("Input sequence (space-separated):     ").split()
 
     if len(sequence) <= 2:
         print("Too few elements in the sequence.")
@@ -17,10 +18,19 @@ def main():
         exponent_expression = "n-1"
         superscript_expression = ''.join(superscript.get(char, char) for char in exponent_expression)
         print(f"Geometric Sequence: {int(ratio)}{superscript_expression} × {int(a1)}")
-
+        want = input("Would you like to get the first 5 values of the given sequence? (y/n):    ")
+        
+        if want == "y":
+            wantNum = int(input("Enter the n amount of time you would like to get the values for:    "))
+            wanted()
         return
 
     print("Neither arithmetic nor geometric sequence detected.")
 
-if __name__ == "__main__":
-    main()
+def wanted():
+    global a1, ratio, wantNum
+    print(f"First {wantNum} term(s):    ", end=" ")
+    sequence_terms = [str(int(ratio) ** (i - 1) * a1) for i in range(1, wantNum)]
+    print(" ".join(sequence_terms))
+
+main()
